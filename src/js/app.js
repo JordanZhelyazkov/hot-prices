814244940e92f2f12365b6f939325a77ca8e071d
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-27 15:09:14 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const priceHot = document.querySelectorAll('p');
+  console.log(priceHot);
+  priceHot.forEach(price => {
+    if(price.className.includes('hot')){
+      price.className += '';
+    }
+  })
 });
